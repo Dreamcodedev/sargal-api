e705e4ff142f3214fcceeb6b25ba9cbe013ded2b
Dreamcodedev <demba.dioop@gmail.com>
2022-06-25 14:38:41 +0200

    add CORS

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'shop',
 ]
 
@@ -49,7 +50,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
+CORS_ALLOW_ALL_ORIGINS : True
+
+CORS_ALLOW_HEADERS = [
+    "*",
+]
+
 
 ROOT_URLCONF = 'project.urls'
 
